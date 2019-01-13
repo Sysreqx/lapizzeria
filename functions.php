863fabeda11d0e6fcac4dfbfb18e6a960100b0d0
sysreqx <sysreqx@gmail.com>
2019-01-13 20:19:20 +0600

    social media:

--- functions.php
+++ functions.php
@@ -3,10 +3,12 @@
 function lapizzeria_styles() {
 	// adding stylesheets
 	wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '6.0.0');
+	wp_register_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.css', array(), '4.7.0');
 	wp_register_style('style', get_template_directory_uri() . '/style.css', array(), '1.0');
 
 	// Enquee the style
 	wp_enqueue_style('normalize');
+	wp_enqueue_style('fontawesome');
 	wp_enqueue_style('style');
 }
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
