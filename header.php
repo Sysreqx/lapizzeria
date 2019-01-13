c84e201c75ce9ed78637531fedf2f97899e8ad6b
sysreqx <sysreqx@gmail.com>
2019-01-13 15:19:17 +0600

    logo

--- header.php
+++ header.php
@@ -6,3 +6,7 @@
 	<?php wp_head(); ?>
 </head>
 <body>
+
+	<header class="site-header">
+		<div class="logo"><a href="<?php echo esc_url(home_url('/') ); ?>"><img src="<?php echo get_template_directory_uri() ?>/img/logo.svg" alt="logo" class="logoimage"></a></div>
+	</header>
