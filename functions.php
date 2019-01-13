2bc55e4a4fc67612960d2f678e30e548f6cd40a9
sysreqx <sysreqx@gmail.com>
2019-01-13 15:46:23 +0600

    Header Menu

--- functions.php
+++ functions.php
@@ -11,3 +11,11 @@ function lapizzeria_styles() {
 }
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
 
+// Add Menus
+function lapizzeria_menus() {
+	register_nav_menus( array(
+		'header-menu' => __('Header Menu', 'lapizzeria')
+	) );
+}
+add_action('init', 'lapizzeria_menus');
+
--- header.php
+++ header.php
@@ -9,4 +9,20 @@
 
 	<header class="site-header">
 		<div class="logo"><a href="<?php echo esc_url(home_url('/') ); ?>"><img src="<?php echo get_template_directory_uri() ?>/img/logo.svg" alt="logo" class="logoimage"></a></div>
+		<!-- 		<div class="header-menu">
+			<?php wp_nav_menu( array('header-menu' => '		Header Menu' )); ?>
+		</div> -->
 	</header>
+	
+	<div class="main-menu">
+		<div class="navigation">
+			<?php 
+			$args = array(
+				'theme_location' => 'header-menu',
+				'container' => 'nav',
+				'container_class' => 'site-nav'
+			);
+			wp_nav_menu($args);
+			?>
+		</div>
+	</div>
