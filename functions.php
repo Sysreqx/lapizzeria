071c2b6feb2a1d9495d9e8b0f5369b596debfaba
sysreqx <sysreqx@gmail.com>
2019-01-16 19:37:44 +0600

    fcking wp loops

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+// Featured Image
+function lapizzeria_setup() {
+	add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'lapizzeria_setup');
+
+
 function lapizzeria_styles() {
 	// adding stylesheets
 	wp_register_style('googlefont', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700|Raleway:400,700,900', array(), '1.0.0');
@@ -19,6 +26,7 @@ function lapizzeria_styles() {
 	wp_enqueue_script('script');
 }
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
+
 
 // Add Menus
 function lapizzeria_menus() {
--- page.php
+++ page.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-	<h1>page.php</h1>
+<!-- Checks content on the page -->
+<?php while(have_posts()): the_post(); ?>
 	
+	<?php the_post_thumbnail(); ?>
+	<h2><?php the_title(); ?></h2>
+	<?php the_content(); ?>
+
+<?php endwhile; ?>
+
 <?php get_footer(); ?>
