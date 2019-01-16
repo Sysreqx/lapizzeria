962e28cc458f8183d089d8e510c07eaa4908b6cc
sysreqx <sysreqx@gmail.com>
2019-01-16 19:48:11 +0600

    image as background

--- page.php
+++ page.php
@@ -2,10 +2,20 @@
 
 <!-- Checks content on the page -->
 <?php while(have_posts()): the_post(); ?>
-	
-	<?php the_post_thumbnail(); ?>
-	<h2><?php the_title(); ?></h2>
-	<?php the_content(); ?>
+
+	<div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+		<div class="hero-content">
+			<div class="hero-text">
+				<h2><?php the_title(); ?></h2>
+			</div>
+		</div>
+	</div>
+
+	<div class="main-content container">
+		<main class="text-center content-text">
+			<?php the_content(); ?>
+		</main>
+	</div>
 
 <?php endwhile; ?>
 
