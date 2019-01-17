8e403ad647d11caed42144604fd829455f2d496d
sysreqx <sysreqx@gmail.com>
2019-01-17 13:44:59 +0600

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,22 @@
-<?php wp_footer(); ?>
+	<footer>
+		<?php 
+		$args = array(
+			'theme-location' => 'header-menu',
+			'container' => 'nav',
+			'after' => '<span class="separator"> | </span>'
+		);
+		wp_nav_menu($args);
+		?>
+
+		<div class="location">
+			<p>8179 Bay Avenue  Mountain View, CA 94043</p>
+			<p>Phone Number: +1-92-456-7890</p>
+		</div>
+
+		<div class="copyright">All rights reserved <?php echo date('Y'); ?></div>
+
+	</footer>
+
+	<?php wp_footer(); ?>
 </body>
 </html>
