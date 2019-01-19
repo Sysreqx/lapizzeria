f09711bde386fb3cc062191b0aadb3501e90f330
sysreqx <sysreqx@gmail.com>
2019-01-19 15:29:51 +0600

    index.php blog posts

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php get_header(); ?>
 
-	<h1>hello from index.php</h1>
+<?php 
+//returns id
+$blog_page = get_option('page_for_posts');
+$image = get_post_thumbnail_id($blog_page);
+// return image
+$image = wp_get_attachment_image_src($image, 'full');
+?>
+
+<!-- Checks content on the page -->
+<div class="hero" style="background-image: url(<?php echo $image[0]; ?>);">
+	<div class="hero-content">
+		<div class="hero-text">
+			<h2><?php get_the_title($blog_page); ?></h2>
+		</div>
+	</div>
+</div>
+
+<div class="main-content container">
+	<main class="text-center content-text">
+		<?php while(have_posts()): the_post(); ?>
+
+		<?php endwhile; ?>
+	</main>
+</div>
+
+
 
 <?php get_footer(); ?>
