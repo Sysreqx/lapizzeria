577f46128742625cdc0f5d7b35bbff900c77f99f
sysreqx <sysreqx@gmail.com>
2019-01-20 14:21:22 +0600

    single poost template

--- single.php
+++ single.php
@@ -1,5 +1,34 @@
 <?php get_header(); ?>
 
-	<h1>hello from single.php</h1>
+<!-- Checks content on the page -->
+<?php while(have_posts()): the_post(); ?>
+
+	<div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+		<div class="hero-content">
+			<div class="hero-text">
+				<h2><?php the_title(); ?></h2>
+			</div>
+		</div>
+	</div>
+
+	<div class="main-content container">
+		<main class="content-text">
+			<div class="entry-information clear">
+				<div class="date">
+					<time>
+						<?php echo the_time('d'); ?>
+						<span><?php echo the_time('M'); ?></span>
+					</time>
+				</div>
+				<p class="author">
+					<i class="fa fa-user" aria-hidden="true"></i>
+					<?php the_author(); ?>
+				</p>
+			</div>
+			<?php the_content(); ?>
+		</main>
+	</div>
+
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
