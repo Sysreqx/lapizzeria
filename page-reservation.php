e28db551f1d9d8fb575a16f9869603c588f92e1c
sysreqx <sysreqx@gmail.com>
2019-01-21 16:36:01 +0600

    styling the reservation from

--- page-reservation.php
+++ page-reservation.php
@@ -17,29 +17,31 @@
 		</div>
 	</div>
 
-	<div class="main-content container">
-		<main class="text-center content-text clear">
-			<form class="reservation-form" method="post">
-				<h2>Make a reservation</h2>
-				<div class="field">
-					<input type="text" name="name" placeholder="Name" required>
-				</div>
-				<div class="field">
-					<input type="datetime-local" name="date" placeholder="Date" required>
-				</div>
-				<div class="field">
-					<input type="email" name="email" placeholder="E-Mail" required>
-				</div>
-				<div class="field">
-					<input type="tel" name="phone" placeholder="Phone Number" required>
-				</div>
+	<div class="main-content container reservation">
+		<main class="content-text clear">
+			<div class="reservation-info">
+				<form class="reservation-form" method="post">
+					<h2>Make a reservation</h2>
+					<div class="field">
+						<input type="text" name="name" placeholder="Name" required>
+					</div>
+					<div class="field">
+						<input type="datetime-local" name="date" placeholder="Date" required>
+					</div>
+					<div class="field">
+						<input type="email" name="email" placeholder="E-Mail" required>
+					</div>
+					<div class="field">
+						<input type="tel" name="phone" placeholder="Phone Number" required>
+					</div>
 
-				<div class="field">
-					<textarea name="message" placeholder="Message" required></textarea>
-				</div>
-				<input type="submit" name="submit" class="button">
-				<input type="hidden" name="hidden" value="1">
-			</form>
+					<div class="field">
+						<textarea name="message" placeholder="Message" required></textarea>
+					</div>
+					<input type="submit" name="submit" class="button" value="Send">
+					<input type="hidden" name="hidden" value="1">
+				</form>
+			</div>
 		</main>
 	</div>
 
