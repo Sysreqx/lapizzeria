39c40154da534a70181e368d0610ac5b741d1109
sysreqx <sysreqx@gmail.com>
2019-01-22 12:43:51 +0600

    adding submenu page

--- inc/options.php
+++ inc/options.php
@@ -1,9 +1,17 @@
 <?php 
 function lapizzeria_options() {
+	 // $page_title, $menu_title, $capability, $menu_slug, $function = '', $icon_url = '', $position = null 
 	add_menu_page( 'La Pizzeria', 'La Pizeeria Options', 'administrator', 'lapizzeria_options', 'lapizzeria_adjustments', '', 20 );
+
+	 // $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function = '' 
+	add_submenu_page( 'lapizzeria_options', 'Reservations', 'Reservations', 'administrator', 'lapizzeria_reservations', 'lapizzeria_reservations' );
 }
 add_action('admin_menu', 'lapizzeria_options');
 
 function lapizzeria_adjustments() {
-	echo 'hello from lapizzeria_adjustments';
+	// Rest of the code
+}
+
+function lapizzeria_reservations() {
+	// Rest of the code
 }
