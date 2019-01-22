c9306227966c36369d5b814052b9b38c72bb0560
sysreqx <sysreqx@gmail.com>
2019-01-22 12:17:22 +0600

    sanitizing data submission and redirect to custom page

--- inc/reservations.php
+++ inc/reservations.php
@@ -2,11 +2,11 @@
 function lapizzeria_save_reservation() {
 	global $wpdb;	
 	if (isset($_POST['reservation']) && $_POST['hidden'] == 1) {
-		$name  = $_POST['name'];
-		$date  = $_POST['date'];
-		$email  = $_POST['email'];
-		$phone  = $_POST['phone'];
-		$message  = $_POST['message'];
+		$name	= sanitize_text_field($_POST['name']);
+		$date	= sanitize_text_field($_POST['date']);
+		$email	= sanitize_email($_POST['email']);
+		$phone	= sanitize_text_field($_POST['phone']);
+		$message	= sanitize_text_field($_POST['message']);
 
 		$table = $wpdb->prefix . 'reservations';
 
@@ -27,6 +27,12 @@ function lapizzeria_save_reservation() {
 		);
 
 		$wpdb->insert($table, $data, $format);
+
+		// Retrieve a page given its title.
+		$url = get_page_by_title('Thanks for your reservation!');
+		// Redirects to another page
+		wp_redirect(get_permalink($url));
+		exit();
 	}
 }
 
